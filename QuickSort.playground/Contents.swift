430af6315e97c35fb86747c017367d5f03298df7
Thorax <thorax@me.com>
2017-03-17 14:18:53 +0300

    fix quicksort

--- QuickSort.playground/Contents.swift
+++ QuickSort.playground/Contents.swift
@@ -4,11 +4,16 @@ import Foundation
 
 extension Array where Element: Comparable {
     func quickSort() -> [Element] {
+        guard count > 1 else { return self }
+        
     let pivot = count / 2
         let value = self[pivot]
-        let less = self.sort { $0 < value }
-        let equal = self.sort { $0 == value }
-        let more = self.sort { $0 > value }
+        let less = self.filter { $0 < value }
+        let equal = self.filter { $0 == value }
+        let more = self.filter { $0 > value }
         return less.quickSort() + equal + more.quickSort()
     }
 }
+
+let arr = [1,4,5,6,3,0,5,4,6,9,34,3,6,5,3,5,6,4,3,6,4,3,4,56,6,7,7,5,4,3]
+let sorted = arr.quickSort()
